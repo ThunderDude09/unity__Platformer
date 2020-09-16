30bb98c6717b229278c6a38cfd5eabb1ca9d951c
Thunder19 <braydenhamous@gmail.com>
2020-09-16 15:35:22 -0500

    Second

---
+++ Platformer/Assets/PlayerMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]
+    float moveSpeed = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        transform.position = new Vector3(transform.position.x + h * Time.deltaTime * moveSpeed,
+                                         transform.position.y,
+                                         transform.position.z + v * Time.deltaTime * moveSpeed);
+    }
+}
